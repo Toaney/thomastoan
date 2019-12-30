15da7bccf114e08547a7dcd9ed8f1e4488f38343
Maj Toan <toaney@gmail.com>
2019-12-30 10:27:57 -0800

    add routing to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import TodoList from "./code-snippets/todo-list.js";
 import Home from "./pages/home.js";
+import Snippets from "./pages/home.js";
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
       <Link className="no-underline header" to="/">Thomas Toan</Link>
     </div>
     <Route exact path="/" component={ Home } />
+    <Route path="/home" component={ Home }
     <Route path="/todo-list" component={ TodoList } />
+    <Route path="/snippets" componetn={ Snippets } />
     </Router>
   );
 }
