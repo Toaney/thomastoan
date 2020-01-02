78e00525d750ca3a695b36921bbf4e4c8fdd9ee2
Maj Toan <toaney@gmail.com>
2020-01-01 21:23:38 -0800

    add snippets structure

--- src/pages/snippets.js
+++ src/pages/snippets.js
@@ -4,20 +4,22 @@ function Snippets() {
     const [displayLink1, setDisplayLink1] = React.useState(false);
 
     return(
-        <div className="snippets-container">
+        <React.Fragment>
             <h2>Code Snippets</h2>
-            {/* //add sidebar component */}
-            <div className="snippets-sidebar">
-                <button class="sidebar-link">
-                    button
-                </button>
+            <div className="snippets-container">
+                {/* //add sidebar component */}
+                <div className="snippets-sidebar">
+                    <button class="sidebar-link">
+                        button
+                    </button> 
+                </div>
+                {/* //add content component */}
+                <div className="snippets-content">
+                    {displayLink1? <div>true</div> : <div>false</div>}
+                    <div class="link-1"></div>
+                </div>
             </div>
-            {/* //add content component */}
-            <div className="snippets-content">
-                {displayLink1? <div>true</div> : <div>false</div>}
-                <div class="link-1"></div>
-            </div>
-        </div>
+        </React.Fragment>
     )
 }
 
